8415400c7ec4a44618498f1040247b88086c06af
NAIRUI (David) GUO <132328885+Wolfred11037@users.noreply.github.com>
2023-06-22 22:28:42 +1000

    Update Budger Data code.py

--- python-challenge/PyBank/Budger Data code.py
+++ python-challenge/PyBank/Budger Data code.py
@@ -1,33 +1,32 @@
 import csv
 
-# Read the CSV file
+# loation of the csv file Budget_ Data
 filename = r"C:\Users\Wolfred\Documents\Data Analysis Boot Camp\Week 3\python-challenge\PyBank\Resources\budget_data.csv"
 with open(filename, 'r') as file:
     csv_reader = csv.reader(file)
     header = next(csv_reader)  # Skip the header row
     data = list(csv_reader)
 
-# Initialize variables
+# delaring tne variables
 total_months = len(data)
 net_total = 0
 changes = []
 max_increase = 0
 max_decrease = 0
 
-# Iterate over the data
 for i in range(total_months):
     date = data[i][0]
     profit_loss = int(data[i][1])
 
-    # Calculate net total
+    # Calculaig the total
     net_total += profit_loss
 
-    # Calculate changes in profit/loss
+    # Calculating changes in profits and losses
     if i > 0:
         change = profit_loss - int(data[i - 1][1])
         changes.append(change)
 
-        # Check for greatest increase/decrease
+        # Checking for the greatest increase and decrease
         if change > max_increase:
             max_increase = change
             max_increase_date = date
@@ -35,10 +34,10 @@ for i in range(total_months):
             max_decrease = change
             max_decrease_date = date
 
-# Calculate average change
+# Calculating the average changes
 average_change = sum(changes) / len(changes)
 
-# Format the results
+# displaying the results as shown in the challenge
 output = f"Financial Analysis\n----------------------------\n"
 output += f"Total Months: {total_months}\n"
 output += f"Total: ${net_total}\n"
@@ -46,5 +45,4 @@ output += f"Average Change: ${average_change:.2f}\n"
 output += f"Greatest Increase in Profits: {max_increase_date} (${max_increase})\n"
 output += f"Greatest Decrease in Profits: {max_decrease_date} (${max_decrease})"
 
-# Print the results
 print(output)
