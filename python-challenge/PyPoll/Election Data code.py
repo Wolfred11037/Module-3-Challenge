de06acd68dc94418372bcbea8549d74cce9bae1d
NAIRUI (David) GUO <132328885+Wolfred11037@users.noreply.github.com>
2023-06-22 22:34:21 +1000

    Update Election Data code.py

--- python-challenge/PyPoll/Election Data code.py
+++ python-challenge/PyPoll/Election Data code.py
@@ -1,43 +1,43 @@
 import csv
 
 def analyze_votes(filename):
-    # Initialize variables
+    # declaing the 'votes' as variables
     total_votes = 0
     candidate_votes = {}
   
-    # Read the CSV file
+    # location of the csv file of 'election data' 
     filename = r"C:\Users\Wolfred\Documents\Data Analysis Boot Camp\Week 3\python-challenge\PyPoll\Resources\election_data.csv"
     with open(filename, 'r') as csvfile:
         csvreader = csv.reader(csvfile)
         
-        # Skip header row
+        # Skips the first row of the csv file which contain the  header
         next(csvreader)
         
-        # Loop through each row
+        # Looping through each of th rows
         for row in csvreader:
-            # Count the total number of votes
+            # calulating the total number of votes
             total_votes += 1
             
-            # Get the candidate's name from the row
+            # Get the elector's name from the row
             candidate_name = row[2]
             
-            # Add the candidate to the dictionary and update the vote count
+            # Add the elector to the dictionary and update the vote total
             if candidate_name in candidate_votes:
                 candidate_votes[candidate_name] += 1
             else:
                 candidate_votes[candidate_name] = 1
   
-    # Calculate the percentage of votes each candidate won
+    # Calculating percentage of votes each candidate has won
     candidate_percentages = {}
     for candidate in candidate_votes:
         votes = candidate_votes[candidate]
         percentage = (votes / total_votes) * 100
         candidate_percentages[candidate] = round(percentage, 2)
   
-    # Find the winner based on popular vote
+    # Find the winner based on popular vote based on the max votings
     winner = max(candidate_votes, key=candidate_votes.get)
   
-    # Print the analysis results
+    # displaying the results as shown in the challenge. 
     print("Election Results")
     print("-------------------------")
     print(f"Total Votes: {total_votes}")
@@ -50,5 +50,4 @@ def analyze_votes(filename):
     print(f"Winner: {winner}")
     print("-------------------------")
 
-# Run the analysis on the provided file
 analyze_votes('election_data.csv')
